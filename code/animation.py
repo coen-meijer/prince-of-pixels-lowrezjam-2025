85def7854ec05b57a244aeda46073b3de34ac660
coen.meijer <coen.meijer@gmail.com>
2025-08-12 15:53:48 +0200

    firts animation!

--- code/animation.py
+++ code/animation.py
@@ -1,31 +1,50 @@
-
+import pygame
 # preconditions: state buttons space
 
-class AnimationPlay
+class AnimationPlayer:
+
+    def __init__(self, animation):
+        self.animation = animation
+        self.frame_index = 0
+        self.mirrored = animation.mirrored
+
+    def get_frame(self):
+        print("frame", self.frame_index, "of", self.animation.frame_count)
+        frame = self.animation.sprites[self.frame_index]
+        self.frame_index += 1
+        if self.mirrored:
+            frame = pygame.transform.flip(frame, flip_x=True, flip_y=False)
+        return frame, self.frame_index >= self.animation.frame_count
 
 class Animation:
 
-    def __init__(self, prestate, buttons, post_state, postcondition, frames):
-        self.preconditions = preconditions
-        self.buttons = buttons
-        self.frames = frames
-        self.post_state = post_state
-        self.postcondition = postcondition
-        self.frames = frames
+    def __init__(self, record):
+        self.name= record["name"]
+        self.start_state = record["start_state"]
+        self.buttons = record["buttons"]
+        self.end_state= record["end_state"]
+        self.name= record["name"]
+        self.mirrored = record["mirrored"]
+        self.frame_count =record["frame_count"]
 
-    def
+        self.sprites = sprites_from_sheet(record["sprite_sheet"],
+                                          record["frame_size"],
+                                          record["frame_count"])
+
+    def get_frame_iterator(self):
+        return AnimationPlayer(self)
 
 
-
-def sprites(spritesheet, sprite_size, frame_count):
-    pygame.image.load(spritesheet).convert_alpha()
+def sprites_from_sheet(sprite_sheet_file, sprite_size, frame_count):
+    sprite_sheet = pygame.image.load(sprite_sheet_file).convert_alpha()
     frames = []
-    sheet_horizontal, sheet_vertical = surf.get_size()
+    sheet_horizontal, sheet_vertical = sprite_sheet.get_size()
     sprite_horizontal, sprite_vertical = sprite_size
     pos_x, pos_y = 0, 0
-    for i in range (frame_count)
-        rect = pygame.Rect((pos_x, pos_y), sprite_size)
-        frame = image.subsurface(rect)
+    for i in range(frame_count):
+        rect = pygame.Rect((pos_y, pos_x), sprite_size)
+        print("trying to cut out frame: ", sprite_sheet_file, rect)
+        frame = sprite_sheet.subsurface(rect)
         frames.append(frame.copy())
 
         pos_x += sprite_horizontal
@@ -33,4 +52,5 @@ def sprites(spritesheet, sprite_size, frame_count):
             pos_x = 0
             pos_y += sprite_vertical
 
+    print("from file", sprite_sheet_file, "generated", len(frames))
     return frames
--- code/pofp.py
+++ code/pofp.py
@@ -1,77 +1,83 @@
 import pygame
 import pygame.gfxdraw
+from pygame import surface
 
 import animation
 from animation import Animation
 
 RESOLUTION = (64, 64)
-COLOR = (64, 64, 64)
+COLOR = (127, 127, 127)
 BLACK = (0, 0, 0)
 
+ANIMATION_FOLDER =  "../animaties"
+
+TURN = ANIMATION_FOLDER + "/lowrez-short-turn-step.png"
+WALK = ANIMATION_FOLDER + "/lowrez-short-walk(2).png"
+STAND = ANIMATION_FOLDER + "/stand.png"
+
 ANIMAION_STATE_MACHINE = {
-    "states" = ["standing", "facing_left", "facing_right"],
-    "first_state": "facing_right"
-    "animations" = [
+    "states" : ["standing", "facing_left", "facing_right"],
+    "first_state": "facing_right",
+    "animations": [
+        {
+            "name": "standing_facing_right",
+            "start_state": "facing_right",
+            "buttons": set(),
+            "end_state": "facing_right",
+            "sprite_sheet": STAND,
+            "frame_size": (8, 8),
+            "frame_count": 1,
+            "mirrored": False
+        },
+        {
+            "name": "standing_facing_left",
+            "start_state": "facing_left",
+            "buttons": set(),
+            "end_state": "facing_left",
+            "sprite_sheet": STAND,
+            "frame_size": (8, 8),
+            "frame_count": 1,
+            "mirrored": True
+        },
         {
             "name": "turn_right",
             "start_state": "facing_left",
-            "buttons": ["right"]
+            "buttons": {"right"},
             "end_state": "facing_right",
-            "animation_sheet": "turning.png",
-            "framesize": (8,8),
+            "sprite_sheet": TURN,
+            "frame_size": (8,8),
             "frame_count": 8,
             "mirrored": False
         },
         {
             "name": "turn_left",
             "start_state": "facing_right",
-            "buttons": ["left"]
+            "buttons": {"left"},
             "end_state": "facing_left",
-            "animation_sheet": "turning.png",
-            "framesize": (8,8),
+            "sprite_sheet": TURN,
+            "frame_size": (8,8),
             "frame_count": 8,
             "mirrored": True
         },
         {
             "name": "step_right",
             "start_state": "facing_right",
-            "buttons": ["right"]
+            "buttons": {"right"},
             "end_state": "facing_right",
-            "animation_sheet": "step.png",
-            "framesize": (8, 8),
+            "sprite_sheet": WALK,
+            "frame_size": (8, 8),
             "frame_count": 4,
             "mirrored": False
         },
         {
             "name": "step_left",
             "start_state": "facing_left",
-            "buttons": ["left"]
+            "buttons": {"left"},
             "end_state": "facing_left",
-            "animation_sheet": "step.png",
-            "framesize": (8, 8),
+            "sprite_sheet": WALK,
+            "frame_size": (8, 8),
             "frame_count": 4,
             "mirrored": True
-
-        },
-        {
-            "name": "standing_facing_left",
-            "start_state": "facing_left",
-            "buttons": []
-            "end_state": "facing_left",
-            "animation_sheet": "stand.png",
-            "framesize": (8, 8),
-            "frame_count": 1,
-            "mirrored": True
-        },
-        {
-            "name": "standing_facing_right",
-            "start_state": "facing_right",
-            "buttons": []
-            "end_state": "facing_right",
-            "animation_sheet": "stand.png",
-            "framesize": (8, 8),
-            "frame_count": 1,
-            "mirrored": False
         },
     ]
 }
@@ -80,19 +86,21 @@ class PofP:
 
     def __init__(self):
         self.canvas = pygame.Surface(RESOLUTION)
-#        print("1", self.canvas)
         self.canvas.fill(COLOR)
         self.pos_x = 32
         self.pos_y = 32
-        self.up_pressed = False
-        self.down_pressed = False
-        self.left_pressed = False
-        self.right_pressed = False
+#        self.up_pressed = False
+#        self.down_pressed = False
+#        self.left_pressed = False
+#        self.right_pressed = False
+        self.buttons_pressed = set()
         self.state = ANIMAION_STATE_MACHINE["first_state"]
+        self.choose_next_animation = True
+
         self.animations = []
-        for animation in ANIMAION_STATE_MACHINE["animations"]:
-
-
+        for animation_info in ANIMAION_STATE_MACHINE["animations"]:
+            self.animations.append(Animation(animation_info))
+        self.currend_animation_player = self.animations[0].get_frame_iterator()
 
     def frame(self, input):
         for event in input:
@@ -101,13 +109,39 @@ class PofP:
                 key_down = (event.type == pygame.KEYDOWN)
                 print(key_down, event.key)
                 if event.key == pygame.K_LEFT:
-                    self.left_pressed = key_down
+                    if key_down:
+                        self.buttons_pressed.add("left")
+                    else:
+                        self.buttons_pressed.remove("left")
                 elif event.key == pygame.K_RIGHT:
-                    self.right_pressed = key_down
+                    if key_down:
+                        self.buttons_pressed.add("right")
+                    else:
+                        self.buttons_pressed.remove("right")
+
+        print(self.buttons_pressed)
+
+        if self.choose_next_animation:
+            print("new animation starts!")
+            state = self.currend_animation_player.animation.end_state
+            print("state:", state, ", buttons:", self.buttons_pressed)
+            for animation in self.animations:
+                print("concidering '", animation.name, "', state_needed:", state )
+                if state == animation.start_state:   # check state
+                    print("state checks out, buttons pressed:", self.buttons_pressed )
+                    if self.buttons_pressed == animation.buttons:
+                        print("chosen:", animation.name)
+                        self.currend_animation_player = animation.get_frame_iterator()
+            print(self.currend_animation_player.animation.name)
+
+ #       print(self.currend_animation_player.animation.name,
+ #             self.currend_animation_player.frame_index,
+ #             self.currend_animation_player.animation.frame_count)
+
+        frame, self.choose_next_animation = self.currend_animation_player.get_frame()
+
+        self.canvas.fill(COLOR)
+        self.canvas.blit(frame, (self.pos_x, self.pos_y))
 
 
-
-#        pygame.gfxdraw.pixel(self.canvas, self.pos_x, self.pos_y, BLACK)
-#        print("3", self.canvas)
         return self.canvas
-        # should return a pygame surface with dimensions 64 x 64
